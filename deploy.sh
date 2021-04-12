4c65ab5cdb5c6aab30c4f76d4a86d0828302015a
hogsmill <info@hogsmill.com>
2021-04-12 16:22:02 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,30 +4,48 @@ if [ "$1" == "-f" ]; then
   FORCE=true
 fi
 
-PORT=3018
-PWD=`pwd`
-APP=`basename $PWD`
-git stash
-GIT=`git pull`
-echo $GIT
-if [ "$FORCE" != "true" -a "$GIT" == "Already up to date." ]; then
-  exit 0
-fi
+APPS=('no-estimates-mobile' 'no-estimates-mobile-new')
+for APP in ${APPS[@]};
+do
+  echo "------------------------------------------------"
+  echo "Installing $APP"
+  echo "------------------------------------------------"
 
-npm install
-npm run build
-rm /var/www/html/$APP/css/*
-rm /var/www/html/$APP/js/*
-cp -R dist/* /var/www/html/$APP
-if [ -f "src/server.js" ]; then
-  SERVER=`ps -ef | grep server.js | grep $PORT | awk {'print $2'}`
-  if [ "$SERVER" != "" ]; then
-    kill -9 $SERVER
-  fi
-  KEEP=`ps -ef | grep keep.sh | grep $PORT | awk {'print $2'}`
-  if [ "$KEEP" != "" ]; then
-    kill -9 $KEEP
-  fi
-fi
+  cd "/usr/apps/$APP"
 
-/usr/bin/php /usr/apps/monitor/src/lib/outdated.php &
+  PORT=3007
+  PWD=`pwd`
+  APP=`basename $PWD`
+  git stash
+  GIT=`git pull`
+  echo $GIT
+  if [ "$FORCE" != "true" -a "$GIT" == "Already up to date." ]; then
+    exit 0
+  fi
+
+  npm install
+  npm run build
+  if [ ! -d /var/www/html/$APP/ ]; then
+    mkdir /var/www/html/$APP
+  fi
+  if [ -d /var/www/html/$APP/css ]; then
+    rm /var/www/html/$APP/css/*
+  else
+    mkdir /var/www/html/$APP/css
+  fi
+  if [ -d /var/www/html/$APP/js ]; then
+    rm /var/www/html/$APP/js/*
+  else
+    mkdir /var/www/html/$APP/js
+  fi
+  cp -R dist/* /var/www/html/$APP
+  if [ -f "src/server.js" ]; then
+    SERVER=`ps -ef | grep server.js | grep "/$APP/" | awk {'print $2'}`
+    if [ "$SERVER" != "" ]; then
+      kill -9 $SERVER
+    fi
+  fi
+
+done
+
+php /usr/apps/monitor/src/lib/outdated.php &

1fde9f7299e3334c30ea10dc3dd75932c5e1af9f
hogsmill <info@hogsmill.com>
2021-09-30 21:25:37 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -61,11 +61,9 @@ do
   fi
 
   cd $DIR
-  if [ "$NEW" == "true" ]
-  then
-    rm $DIR/package-lock.json
-    rm -rf $DIR/node_modules
-  fi
+
+  rm $DIR/package-lock.json
+  rm -rf $DIR/node_modules
 
   PWD=`pwd`
   APP=`basename $PWD`
@@ -98,7 +96,8 @@ do
       kill -9 $SERVER
     fi
   fi
-
+  rm -rf $DIR/node_modules/.cache
+  rm -rf $DIR/dist
 done
 
 ps -ef | grep php | grep outdated
